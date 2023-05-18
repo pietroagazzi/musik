acc10b8d700c1a6d3c6e26098ba0dff82efc52ff
Pietro Agazzi <agazzi_pietro@protonmail.com>
2023-05-18 14:58:00 +0200

    add csrf token for follow

--- assets/controllers/follow_controller.js
+++ assets/controllers/follow_controller.js
@@ -2,51 +2,70 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
     toggle(event) {
+        // get the action url
         const followUrl = event.target.dataset.followUrlValue;
-        const isFollowing = event.target.dataset.following;
 
+        // get the isFollowing attribute (0 if not following, 1 if following)
+        const isFollowing = event.target.dataset.followingValue;
+
+        // get the csrf token
+        const csrfToken = event.target.dataset.csrfValue;
 
         if (isFollowing === '1') {
-            this._unfollow(followUrl)
+            this._unfollow(followUrl, csrfToken)
                 .then(response => {
                     if (response.status !== 200) {
                         throw new Error('Something went wrong: ' + response.statusText);
                     }
+
+                    return response.json();
                 })
-                .then(() => {
+                .then(data => {
+                    let token = data.csrf_token;
+
+                    event.target.setAttribute('data-csrf-value', token);
+
                     // update isFollowing attribute
-                    event.target.setAttribute('data-following', '0');
+                    event.target.setAttribute('data-following-value', '0');
                     // update text content
                     event.target.textContent = 'Follow';
-                })
+                });
         } else {
-            this._follow(followUrl)
+            this._follow(followUrl, csrfToken)
                 .then(response => {
                     if (response.status !== 200) {
                         throw new Error('Something went wrong: ' + response.statusText);
                     }
+
+                    return response.json();
                 })
-                .then(() => {
+                .then(data => {
+                    let token = data.csrf_token;
+
+                    event.target.setAttribute('data-csrf-value', token);
+                    
                     // update isFollowing attribute
-                    event.target.setAttribute('data-following', '1');
+                    event.target.setAttribute('data-following-value', '1');
                     // update text content
                     event.target.textContent = 'Unfollow';
-                })
+                });
         }
     }
 
-    _follow(followUrl) {
+    _follow(followUrl, csrfToken) {
         return fetch(followUrl, {
             method: 'POST', headers: {
-                'X-Requested-With': 'XMLHttpRequest', // 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                'X-Requested-With': 'XMLHttpRequest',
+                'x-csrf-token': csrfToken
             }
         })
     }
 
-    _unfollow(followUrl) {
+    _unfollow(followUrl, csrfToken) {
         return fetch(followUrl, {
             method: 'DELETE', headers: {
-                'X-Requested-With': 'XMLHttpRequest', // 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                'X-Requested-With': 'XMLHttpRequest',
+                'x-csrf-token': csrfToken
             }
         })
     }
--- src/Controller/Api/Action/ApiActionController.php
+++ src/Controller/Api/Action/ApiActionController.php
@@ -6,8 +6,11 @@ use App\Entity\Follow;
 use App\Entity\User;
 use App\Repository\FollowRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -23,70 +26,65 @@ class ApiActionController extends AbstractController
 	}
 
 	/**
+	 * @param Request $request
 	 * @param int $user_id
 	 * @param User|null $user
 	 * @return Response
+	 * @throws ContainerExceptionInterface
+	 * @throws NotFoundExceptionInterface
 	 */
-	#[Route('/user/{user_id}/follow', name: 'follow', requirements: ['user_id' => '\d+'], methods: ['POST'])]
-	public function index(int $user_id, ?UserInterface $user): Response
+	#[Route('/user/{user_id}/follow', name: 'follow', requirements: ['user_id' => '\d+'], methods: ['POST', 'DELETE'])]
+	public function index(Request $request, int $user_id, ?UserInterface $user): Response
 	{
 		if (!$user) {
 			return new JsonResponse(status: Response::HTTP_UNAUTHORIZED);
 		}
 
+		// check csrf token
+		$csrfToken = $request->headers->get('X-CSRF-Token');
+
+		if (!$this->isCsrfTokenValid('api_action', $csrfToken)) {
+			return new JsonResponse(['message' => 'Invalid CSRF token'], 406);
+		}
+
+		// get user to follow
+		$followed = $this->entityManager->getRepository(User::class)->find($user_id);
+
+		// check if user exists
+		if (!$followed) {
+			return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+		}
+
+		// check if user is not trying to follow himself
 		if ($user->getId() === $user_id) {
 			return new JsonResponse(['message' => 'You can\'t follow yourself'], Response::HTTP_BAD_REQUEST);
 		}
 
-		$followed = $this->entityManager->getRepository(User::class)->find($user_id);
-
-		if (!$followed) {
-			return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
-		}
-
+		// get follow repository
 		/** @var FollowRepository $followerRepository */
 		$followerRepository = $this->entityManager->getRepository(Follow::class);
 
-		if ($followed->followedBy($user)) {
+		// check if user already follow this user on POST request
+		if ($followed->followedBy($user) and $request->isMethod('POST')) {
 			return new JsonResponse(['message' => 'You already follow this user'], Response::HTTP_BAD_REQUEST);
 		}
 
-		$followerRepository->follow($user, $followed);
-
-		return new JsonResponse(['message' => 'Followed'], Response::HTTP_OK);
-	}
-
-	/**
-	 * @param int $user_id
-	 * @param User|null $user
-	 * @return Response
-	 */
-	#[Route('/user/{user_id}/follow', name: 'unfollow', requirements: ['user_id' => '\d+'], methods: ['DELETE'])]
-	public function unfollow(int $user_id, ?UserInterface $user): Response
-	{
-		if (!$user) {
-			return new JsonResponse(status: Response::HTTP_UNAUTHORIZED);
-		}
-
-		if ($user->getId() === $user_id) {
-			return new JsonResponse(['message' => 'You can\'t unfollow yourself'], Response::HTTP_BAD_REQUEST);
-		}
-
-		$followed = $this->entityManager->getRepository(User::class)->find($user_id);
-
-		if (!$followed) {
-			return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
-		}
-
-		/** @var FollowRepository $followerRepository */
-		$followerRepository = $this->entityManager->getRepository(Follow::class);
-
-		if (!$followed->followedBy($user)) {
+		// check if user don't follow this user on DELETE request
+		if (!$followed->followedBy($user) and $request->isMethod('DELETE')) {
 			return new JsonResponse(['message' => 'You don\'t follow this user'], Response::HTTP_BAD_REQUEST);
 		}
 
-		$followerRepository->unfollow($user, $followed);
+		// follow or unfollow user
+		if ($request->isMethod('DELETE')) {
+			$followerRepository->unfollow($user, $followed);
+		} elseif ($request->isMethod('POST')) {
+			$followerRepository->follow($user, $followed);
+		}
 
-		return new JsonResponse(['message' => 'Unfollowed'], Response::HTTP_OK);
+		// generate new csrf token
+		$tokenProvider = $this->container->get('security.csrf.token_manager');
+		$token = $tokenProvider->getToken('api_action')->getValue();
+
+		return new JsonResponse(['csrf_token' => $token], Response::HTTP_OK);
 	}
 }
